c9bedf4a66ea8ebca344999f4f23e0083c205bc0
JAKARIA8785 <ocjakaria8785@gmail.com>
2025-02-12 00:05:56 +0600

    init commit

--- src/Morsecode/AEScode.java
+++ src/Morsecode/AEScode.java
@@ -4,13 +4,69 @@
  */
 package Morsecode;
 
-/**
- *
- * @author TANVIR JAKARIA LITU
- */
-public class AEScode {
-    public AEScode(){
-        int c=5;
+/*
 
+  @author TANVIR JAKARIA LITU
+ */
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Scanner;
+/**
+ * Possible KEY_SIZE values are 128, 192 and 256
+ * Possible T_LEN values are 128, 120, 112, 104 and 96
+ */
+
+public class AEScode {
+    private SecretKey key;
+    private int KEY_SIZE = 128;
+    private int T_LEN = 128;
+    private byte[] IV;
+
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+    }
+
+    public void initFromStrings(String secretKey, String IV) {
+        key = new SecretKeySpec(decode(secretKey), "AES");
+        this.IV = decode(IV);
+    }
+
+    public String encrypt(String message) throws Exception {
+        byte[] messageInBytes = message.getBytes();
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, IV);
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key, spec);
+        byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        return encode(encryptedBytes);
+    }
+
+
+    private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            AEScode aes = new AEScode();
+            aes.initFromStrings("CHuO1Fjd8YgJqTyapibFBQ==", "e3IYYJC2hxe24/EO");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Your Text:");
+            String input = scanner.nextLine();
+            String encryptedMessage = aes.encrypt(input);
+            scanner.close();
+            System.err.println("Encrypted Message : " + encryptedMessage);
+        } catch (Exception ignored) {
+        }
     }
 }
