1770720104de11dd0fda9114335f29c01444d7f7
JAKARIA8785 <ocjakaria8785@gmail.com>
2025-03-10 13:06:44 +0600

    init commit

---
+++ src/application/form/other/ROT13/ROT13decode.java
@@ -0,0 +1,170 @@
+package application.form.other.ROT13;
+
+import application.form.other.Base64.Base64Show;
+import Logics.ROT13Example;
+import static Logics.ROT13Example.rot13;
+import MiscItem.BACKGOUND.PanelCustom;
+import MiscItem.swing.FileLoader;
+import com.formdev.flatlaf.FlatClientProperties;
+import java.awt.Color;
+import java.awt.Insets;
+
+import raven.alerts.MessageAlerts;
+import raven.toast.Notifications;
+
+
+public class ROT13decode extends PanelCustom {
+
+
+
+    public ROT13decode() {
+        initComponents();
+        setAlpha(1);
+        TxtInput.setLineWrap(true);
+        TxtInput.setWrapStyleWord(true);
+        TxtInput.setEditable(true);
+        TxtInput.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Enter your Morse Code here");
+        TxtInput.putClientProperty(FlatClientProperties.STYLE, ""+ "font: $h2.font;");
+        
+        TScroll.putClientProperty("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+        TScroll.putClientProperty("ScrollBar.track", Color.DARK_GRAY);
+        TScroll.putClientProperty("ScrollBar.trackArc", 999);
+        TScroll.putClientProperty("ScrollBar.thumbArc", 999);
+        TScroll.putClientProperty("ScrollBar.trackInsets", new Insets(2, 4, 2, 4) );
+
+        TxtOut.setLineWrap(true);
+        TxtOut.setWrapStyleWord(true);
+        TxtOut.setEditable(false);
+        TxtOut.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Enter your Txt will appear here");
+        TxtOut.putClientProperty(FlatClientProperties.STYLE, ""+ "font: $h2.font;");
+
+        OScroll.putClientProperty("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+        OScroll.putClientProperty("ScrollBar.track", Color.DARK_GRAY);
+        OScroll.putClientProperty("ScrollBar.trackArc", 999);
+        OScroll.putClientProperty("ScrollBar.thumbArc", 999);
+        OScroll.putClientProperty("ScrollBar.trackInsets", new Insets(2, 4, 2, 4) );
+
+        Title.putClientProperty(FlatClientProperties.STYLE, ""+ "font: $h1.font;");
+        Title.setForeground(Color.BLACK);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        TScroll = new javax.swing.JScrollPane();
+        TxtInput = new javax.swing.JTextArea();
+        OScroll = new javax.swing.JScrollPane();
+        TxtOut = new javax.swing.JTextArea();
+        Convert = new javax.swing.JButton();
+        Title = new javax.swing.JLabel();
+        Load = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(216, 164, 127));
+        setToolTipText("");
+        setAutoscrolls(true);
+
+        TxtInput.setColumns(20);
+        TxtInput.setRows(5);
+        TScroll.setViewportView(TxtInput);
+
+        TxtOut.setColumns(20);
+        TxtOut.setRows(5);
+        OScroll.setViewportView(TxtOut);
+
+        Convert.setText("CONVERT");
+        Convert.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ConvertActionPerformed(evt);
+            }
+        });
+
+        Title.setText("ROT13 DECRYPTION");
+        Title.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        Load.setText("LOAD");
+        Load.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LoadActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(Convert, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Load, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(OScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(TScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(Title, javax.swing.GroupLayout.PREFERRED_SIZE, 336, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(Title, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(OScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Convert, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Load, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(32, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void ConvertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConvertActionPerformed
+         if(TxtInput.getText().equals("")){
+            Notifications.getInstance().show(Notifications.Type.ERROR, Notifications.Location.TOP_LEFT, "Please enter your text");
+        }
+        else {
+             if(Base64Show.logic==null){
+                 MessageAlerts.getInstance().showMessage("DATA SERVER ERROR", "Error while loading RSA. We are sorry for this unwanted error. You are requested to try again or You can contact with admin. Thank you.", MessageAlerts.MessageType.ERROR);
+             }
+             else {
+                
+                 String input = TxtInput.getText();
+
+        // Apply ROT13 encoding/decoding
+        String result = rot13(input);
+
+        // Output the result
+        TxtOut.setText(result);
+        
+             }
+        
+        }
+    }//GEN-LAST:event_ConvertActionPerformed
+
+    private void LoadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoadActionPerformed
+    
+        FileLoader.loadFile("Open File For RSA Decryption", TxtInput, this);
+    
+        // TODO add your handling code here:
+   }//GEN-LAST:event_LoadActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Convert;
+    private javax.swing.JButton Load;
+    private javax.swing.JScrollPane OScroll;
+    private javax.swing.JScrollPane TScroll;
+    private javax.swing.JLabel Title;
+    private javax.swing.JTextArea TxtInput;
+    private javax.swing.JTextArea TxtOut;
+    // End of variables declaration//GEN-END:variables
+}
